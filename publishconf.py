045a6c541b8fd0c867174ef8cc98086ed7b355d3
Guilherme Baptista <guisgb13@gmail.com>
2014-04-30 03:11:44 +0200

    Updating SITE URL so that new.buddycloud.org can load resources correctly

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://xmpp.org'
+SITEURL = 'http://new.xmpp.org'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
