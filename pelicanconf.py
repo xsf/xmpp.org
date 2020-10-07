xsf/xmpp.org

commit 92a49761ff27e09a8f4ba9a333073ce045783762

Config: Set SITEURL for Feeds

pelicanconf.py
@@ -6,7 +6,7 @@ import logging
 import json
 
 SITENAME = "XMPP"
-SITE_URL = ""
+SITEURL = "https://xmpp.org"
 TIMEZONE = "Europe/Paris"  #Unused (Pelican complains if you don't provide it)
 DEFAULT_LANG = "en"
 

commit 696ec128134d3596aea3420567927c9f3e37d210

Fix setting of SITE_URL
This fixes a regression introduced in 92a49761 which broke the
website build. The change was introduced in PR #809.

pelicanconf.py
@@ -6,7 +6,7 @@ import logging
 import json
 
 SITENAME = "XMPP"
-SITEURL = "https://xmpp.org"
+SITE_URL = "https://xmpp.org"
 TIMEZONE = "Europe/Paris"  #Unused (Pelican complains if you don't provide it)
 DEFAULT_LANG = "en"
 