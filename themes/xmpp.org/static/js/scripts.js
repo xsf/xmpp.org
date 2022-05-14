09bb3b7275d9609965fe26300e50dcf36ad80f21
wurstsalat <mailtrash@posteo.de>
2022-05-14 02:20:56 +0200

    Software list: Select tab for platform reported by user agent

---
+++ themes/xmpp.org/static/js/scripts.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    // Software list: Select tab for platform reported by user agent
+    const clients_list = document.getElementById("pills-software-content");
+    const select_tab = function(os_name) {
+      var tab_trigger_element = document.querySelector('#' + os_name+ '-tab');
+      var tab = new bootstrap.Tab(tab_trigger_element);
+      tab.show();
+    };
+    if (clients_list) {
+      if (navigator.userAgent.indexOf("Android") >= 0) {
+        select_tab("android");
+      } else if (navigator.userAgent.indexOf("Linux") >= 0) {
+        select_tab("linux");
+      } else if (navigator.userAgent.indexOf("iPhone") >= 0) {
+        select_tab("ios");
+      } else if (navigator.userAgent.indexOf("Windows") >= 0) {
+        select_tab("windows");
+      } else if (navigator.userAgent.indexOf("Macintosh") >= 0) {
+        select_tab("macos");
+      } else {
+        select_tab("other");
+      }
+    }
+};
