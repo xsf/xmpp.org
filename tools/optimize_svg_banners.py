d52ab411d6f2ee03d0c98af871d95ecf311d3c1f
wurstsalat <mailtrash@posteo.de>
2025-04-28 21:48:38 +0200

    cq: SVG optimizer: Fix formatting

--- tools/optimize_svg_banners.py
+++ tools/optimize_svg_banners.py
@@ -11,43 +11,47 @@ https://stackoverflow.com/questions/55812554/try-using-scour-to-clean-an-svg
 https://github.com/scour-project/scour/blob/master/scour/scour.py#L3623
 """
 
+from typing import cast
+
 import argparse
 import sys
-
-from lxml import etree
 from pathlib import Path
+
+from lxml import etree  # type: ignore  # noqa: PGH003
 from scour import scour
+
 
 def process() -> None:
     """Processes svg file to minimize size"""
-    parser = argparse.ArgumentParser(
+    arg_parser = argparse.ArgumentParser(
         description="Minimize size of svg file.",
     )
-    parser.add_argument(
+    arg_parser.add_argument(
         "filename",
         type=str,
-        nargs='+',
+        nargs="+",
         help="Path to svg file, it must end in _source.svg.",
     )
-    args = parser.parse_args()
-    input_file = args.filename[0]
-    if input_file[-11:] != "_source.svg":
-         print("Filename should end with '_source.svg'")
-         sys.exit(1)
+    args = arg_parser.parse_args()
+    input_file_path = cast("str", args.filename[0])
+    if not input_file_path.endswith("_source.svg"):
+        print("Filename should end with '_source.svg'")
+        sys.exit(1)
 
     print("Processing...")
-    "with Path(input_file).open("r") as f:"
-    "    svg = f.read()"
-    tree = etree.parse(input_file)
-    print("Removing text fileds")
-    targets = tree.xpath('//x:text',namespaces={'x': 'http://www.w3.org/2000/svg'})
+    tree = etree.parse(input_file_path)
+
+    print("Removing text fields...")
+    targets = tree.xpath("//x:text", namespaces={"x": "http://www.w3.org/2000/svg"})
     for target in targets:
         target.getparent().remove(target)
+
     svg = etree.tostring(tree).decode()
-    print("Minimizing")
+
+    print("Minimizing...")
     clean_svg = scour.scourString(svg)
-    output_file = input_file.replace("_source.svg",".svg")
-    with Path(output_file).open("w", encoding="utf-8") as f:
+    output_file_path = input_file_path.replace("_source.svg", ".svg")
+    with Path(output_file_path).open("w", encoding="utf-8") as f:
         f.write(clean_svg)
     print("All done!")
 
